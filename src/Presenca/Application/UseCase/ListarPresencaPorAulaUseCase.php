fa475c52333c32de5ac09c6b69f23db3cad25328
iohanjheremy <iohanjheremy11@gmail.com>
2025-08-05 10:19:07 -0400

    Tratamento de erros no PresencaController

--- src/Presenca/Application/UseCase/ListarPresencaPorAulaUseCase.php
+++ src/Presenca/Application/UseCase/ListarPresencaPorAulaUseCase.php
@@ -3,15 +3,32 @@
 
 namespace App\Presenca\Application\UseCase;
 
+use App\Aula\Domain\Repository\AulaRepositoryInterface;
 use App\Presenca\Domain\Repository\PresencaRepositoryInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ListarPresencaPorAulaUseCase 
 {
-    public function __construct(private PresencaRepositoryInterface $presencaRepository)
-    {}
+    private PresencaRepositoryInterface $presencaRepository;
+    private AulaRepositoryInterface $aulaRepository;
+
+    public function __construct(
+        PresencaRepositoryInterface $presencaRepository,
+        AulaRepositoryInterface $aulaRepository
+    ) {
+        $this->presencaRepository = $presencaRepository;
+        $this->aulaRepository = $aulaRepository;
+    }
 
     public function execute (int $aulaId): array
     {
+
+        $aula = $this->aulaRepository->find($aulaId);
+
+        if (!$aula) {
+            throw new NotFoundHttpException('Aula não encontrada');
+        }
+
         return $this->presencaRepository->findByAulaId($aulaId);
     }
 }
